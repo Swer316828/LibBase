1e69b11852e9aa3fe45986780d896e7e6c71cc64
sunfeihu <409732847@qq.com>
2018-05-29 11:19:14 +0800

    compileSdkVersion = 27 supportVersion = '27.1.0'

--- app/src/main/java/com/sfh/base/MainActivity.java
+++ app/src/main/java/com/sfh/base/MainActivity.java
@@ -3,11 +3,19 @@ package com.sfh.base;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.sfh.lib.mvp.IPresenter;
+import com.sfh.lib.ui.AbstractActivity;
+
+public class MainActivity extends AbstractActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public IPresenter getPresenter() {
+        return null;
     }
 }
