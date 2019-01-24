92dde105af812cdefff4a39c4b874153ad606072
sunfeihu <316828>
2019-01-24 14:04:13 +0800

    优化HTTP2
    Signed-off-by: sunfeihu <316828>

--- lib/src/main/java/com/sfh/lib/http/service/AbstractHttpClientService.java
+++ lib/src/main/java/com/sfh/lib/http/service/AbstractHttpClientService.java
@@ -36,6 +36,10 @@ public abstract class AbstractHttpClientService implements IRxHttpClient {
         UtilLog.setDEBUG (this.log ());
     }
 
+    @Override
+    public <T> T getRxHttpService(Class<T> service){
+        return this.mClientBuilder.builder (service);
+    }
 
     @Override
     public OkHttpClient getHttpClientService() {
